3708d6be66c69ba480466a8a69e9aa4d53cd3753
Nadezhda Traykova <n.l.traykova@gmail.com>
2026-08-06 21:44:24 +0300

    add logout functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -52,4 +52,10 @@ authController.post('/login', async (req, res) => {
     res.redirect('/');
 });
 
+authController.get('/logout', (req, res) => {
+    res.clearCookie('auth');
+
+    res.redirect('/');
+});
+
 export default authController;
